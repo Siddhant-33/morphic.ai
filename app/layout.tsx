95dc4a10a1a733c917933b2e8861b0e533978a0a
Siddhant ray <siddhant.ray1589@gmail.com>
2026-05-25 15:33:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import Script from 'next/script'
 
 import { Analytics } from '@vercel/analytics/next'
 
@@ -100,7 +99,6 @@ export default async function RootLayout({
           <Toaster />
           <Analytics />
         </ThemeProvider>
-        <Script src="https://checkout.razorpay.com/v1/checkout.js" />
       </body>
     </html>
   )
